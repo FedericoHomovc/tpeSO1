0c5a30add1172140bc9899c91834c44b1eba1e89
jose <jose@jose.(none)>
2011-09-12 21:06:42 -0300

    structures commentaries

--- include/structs.h
+++ include/structs.h
@@ -1,6 +1,6 @@
 /***
 ***
-***		structs.h
+***				structs.h
 ***				Jose Ignacio Galindo
 ***				Federico Homovc
 ***				Nicolas Loreti
@@ -8,11 +8,27 @@
 ***
 ***/
 
+/*
+ * name: medicine
+ * description: stands for a package of certain medicine
+ * @name: name of the medicine
+ * @quantity: quantity of the medicine
+ *
+ */
 typedef struct{
    char * name;
    int quantity;
 }medicine;
 
+/*
+ * name: city
+ * description: city representation
+ * @name: name of the city
+ * @medicines: array with the city medicine requirements
+ * @ID: identification of the city
+ * @medCount:lenght of the medicine array
+ *
+ */
 typedef struct{
    char * name;
    medicine ** medicines;
@@ -20,6 +36,16 @@ typedef struct{
    unsigned medCount;
 }city;
 
+/*
+ * name: plane
+ * description: plane representation.
+ * @medicines: array with the medicines stored in the plane.
+ * @startCity: departure city of the plane.
+ * @destinationID: ID of the city of destination.
+ * @medCount:lenght of the medicine array.
+ * @companyID: ID of the company of the plane.
+ * @planeID: ID of the plane.
+ */
 typedef struct{
    medicine ** medicines;
    char * startCity;
@@ -30,12 +56,28 @@ typedef struct{
    unsigned planeID;
 }plane;
 
+/*
+ * name: company
+ * description: company representation
+ * @companyPlanes: array of planes of the company.
+ * @planesCount:plane array length.
+ * @ID: ID of the company.
+ *
+ */
 typedef struct{
    plane ** companyPlanes;
    unsigned planesCount;
    unsigned ID;
 }company;
 
+/*
+ * name: map
+ * description: map representation.
+ * @cities: array of cities.
+ * @graph: matrix with the distances from city to city.
+ * @citiesCount: quantity of cities.
+ * @companiesCount: quantity of companies.
+ */
 typedef struct{
    city ** cities;
    int ** graph;
